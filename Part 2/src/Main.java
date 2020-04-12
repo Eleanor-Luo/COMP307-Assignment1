7c332f45f594e4d57d2b870512760ed50bb15706
Braden Roberts-Letiu <bradenrletiu@gmail.com>
2020-04-13 11:12:15 +1200

    Added Part 2 Data

--- Part 2/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            Log.error(
-                    "Incorrect number of arguments! \nCorrect Usage: java main <path to training set> <path to test set>");
-        }
-    }
-}
---
+++ Part 2/src/Part2.java
@@ -0,0 +1,9 @@
+
+public class Part2 {
+
+    public Part2(File )
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("DA FUK");
+    }
+}
